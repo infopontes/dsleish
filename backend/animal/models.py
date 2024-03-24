32062027787931784901575982166a74bf499d86
Marcelo Pontes <infopontes@gmail.com>
2024-03-24 14:29:38 -0300

    Changed layout label app Animal.

--- backend/animal/models.py
+++ backend/animal/models.py
@@ -15,7 +15,6 @@ class Animal(TimeStampedModel):
     SEXO_CHOICES = (
         ("F", "Feminino"),
         ("M", "Masculino"),
-        ("N", "Nenhuma das opções")
     )
     
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
